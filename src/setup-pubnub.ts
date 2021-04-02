747e3d0f995aea8446b3f1218ab10ca9d83b7bd7
Tyler Long <tyler4long@gmail.com>
2021-04-02 10:39:38 -0700

    Add PubNub support

---
+++ src/setup-pubnub.ts
@@ -0,0 +1,33 @@
+import RingCentral from '@rc-ex/core';
+import PubNubExtension from '@rc-ex/pubnub';
+import Rest from '@rc-ex/core/lib/Rest';
+import waitFor from 'wait-for-async';
+
+const rc = new RingCentral({
+  server: Rest.productionServer,
+  token: {
+    access_token: process.env.RINGCENTRAL_CHATBOT_TOKEN,
+  },
+});
+const pubNubExtension = new PubNubExtension();
+
+(async () => {
+  console.log('setup PubNub');
+  await rc.installExtension(pubNubExtension);
+  const subscription = await pubNubExtension.subscribe(
+    [
+      '/restapi/v1.0/glip/posts',
+      '/restapi/v1.0/glip/groups',
+      '/restapi/v1.0/account/~/extension/~',
+    ],
+    message => {
+      console.log('*** PubNub message start ***');
+      console.log(message);
+      console.log('*** PubNub message end ***');
+    }
+  );
+  console.log(JSON.stringify(subscription, null, 2));
+  console.log('done');
+
+  await waitFor({interval: 999999999});
+})();
